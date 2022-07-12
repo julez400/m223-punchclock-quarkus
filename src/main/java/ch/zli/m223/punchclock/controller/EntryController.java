11a03d2659dd134c0bfb9ca1da266b102e36b1bd
julez400 <julian.r.wittmann@gmail.com>
2022-07-12 13:19:05 +0200

    Delete Button

--- src/main/java/ch/zli/m223/punchclock/controller/EntryController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/EntryController.java
@@ -9,6 +9,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -42,13 +43,12 @@ public class EntryController {
 
     @DELETE
     @Operation(summary = "Remove a existing Entry", description = "Haha funny deleted")
-    public void remove(long id) {
+    @Path("/{id}")
+    public void remove(@PathParam("id") long id) {
         entryService.remove(id);
     }
 
     @PUT
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
     @Operation(summary = "Updates a existing Entry", description = "Haha new funny")
     public Entry update(Entry entry) {
         return entryService.update(entry);
