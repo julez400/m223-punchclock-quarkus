49f73d5c5c2fc22911a117ae22313f184b371b4e
julez400 <julian.r.wittmann@gmail.com>
2022-07-13 16:40:06 +0200

    Implemented Authentication & CategoryService

--- src/main/java/ch/zli/m223/punchclock/service/AuthenticationService.java
+++ src/main/java/ch/zli/m223/punchclock/service/AuthenticationService.java
@@ -5,12 +5,32 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 import io.smallrye.jwt.build.Jwt;
 import org.eclipse.microprofile.jwt.Claims;
 
+import ch.zli.m223.punchclock.domain.User;
+
 @RequestScoped
 public class AuthenticationService {
+
+    private EntityManager entityManager;
+    
+    @Inject
+    public boolean checkIfUserExists(User user){        
+    var query = entityManager.createQuery("SELECT COUNT(*) FROM User WHERE username = :name AND password = :password");        
+    query.setParameter("name", user.getUsername());
+    query.setParameter("password", user.getPassword());
+    var result = query.getSingleResult();
+
+    if((long)result == 1) 
+    {
+        return true;
+    }
+    return false;
+}
     
     public String GenerateValidJwtToken(String username){
         String token =
@@ -24,3 +44,7 @@ public class AuthenticationService {
     }
 
 }
+
+
+
+
